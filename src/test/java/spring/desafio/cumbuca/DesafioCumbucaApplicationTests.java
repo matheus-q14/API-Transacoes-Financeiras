e544fc83f98a7e03937c8c2a3fa77eaf46f17aeb
Matheus <matheusquerino91@gmail.com>
2025-04-17 15:31:55 -0300

    commit inicial

---
+++ src/test/java/spring/desafio/cumbuca/DesafioCumbucaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.desafio.cumbuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCumbucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
